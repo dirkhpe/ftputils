f13aff2febcdcf0087c186242e8c307179d767e3
Dirk Vermeylen <dirk.vermeylen@skynet.be>
2020-04-09 09:11:49 +0200

    List files

---
+++ db_list.py
@@ -0,0 +1,17 @@
+#!/home/mowdr/env/dataroom/bin/python
+"""
+This script will moves the database tables to the dropserver.
+"""
+from lib import my_env
+from lib import sftp_key
+
+cfg = my_env.init_env("ftputils", __file__)
+props = dict(
+    host=cfg['DBDump']["host"],
+    user=cfg['DBDump']["user"],
+    pkf=cfg['DBDump']["pkf"]
+)
+remote = sftp_key.SftpHandler(**props)
+res = remote.listdir()
+print(res)
+remote.close_connection()
